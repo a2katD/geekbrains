c98ced29da656c10884e598f5c7ec88d7e2f4ce8
a2kat <mousedrw@ya.ru>
2021-01-22 13:42:03 +0300

    push homework lesson3

---
+++ lesson_3/task3_6.py
@@ -0,0 +1,20 @@
+# Реализовать функцию int_func(), принимающую слова из маленьких латинских букв
+# и возвращающую их же, но с прописной первой буквой.
+# Например, print(int_func(‘text’)) -> Text.
+
+# Данный код работает как с 1 словом, так и со строкой разделенной пробелами
+def int_func_title(word):
+    return word.title()
+
+print(int_func_title(input("Введите слово:\n")))
+
+# Без title
+def int_func(string):
+    string_list = string.split()
+    for i in range(len(string_list)):
+        if string_list[i][0].isalpha():
+            string_list[i] = string_list[i][0].upper() + string_list[i][1:]
+    string = " ".join(string_list)
+    return string
+
+print(int_func(input("Введите предложение:\n")))
