4e51a26e8ca96e5e46e5fb83134e8d4c83974d80
a2kat <mousedrw@ya.ru>
2021-01-25 12:02:44 +0300

    task3_4 +loop on error

--- lesson_3/task3_4.py
+++ lesson_3/task3_4.py
@@ -17,12 +17,13 @@ def my_func_cycle(x, y):
     else:
         return 1 / rez
 
-
-try:
-    x = int(input("Введите число x: "))
-    y = int(input("Введите число y: "))
-except:
-    print("Ошибка, вы ввели не число")
-else:
-    print(my_func_star(x, y))
-    print(my_func_cycle(x, y))
+while True:
+    try:
+        x = float(input("Введите действительное положительное число x: "))
+        y = int(input("Введите целое отрицательное число y: "))
+    except:
+        print("Ошибка, вы ввели не число")
+    else:
+        print(my_func_star(x, y))
+        print(my_func_cycle(x, y))
+        break
