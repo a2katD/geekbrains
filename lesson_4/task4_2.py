f1283ff56a346bca6dce33cc4299c612b6e4f89c
a2kat <mousedrw@ya.ru>
2021-01-27 14:59:28 +0300

    task4_2 +if index != 0

--- lesson_4/task4_2.py
+++ lesson_4/task4_2.py
@@ -4,10 +4,9 @@
 # Для его формирования используйте генератор.
 
 from random import randint
-from itertools import count
 
 rand_list = [randint(0, 100) for i in range(10)]
 print(f"Изначальный список {rand_list}")
 
-new_list = [value for index, value in enumerate(rand_list) if value > rand_list[index - 1]]
+new_list = [value for index, value in enumerate(rand_list) if value > rand_list[index - 1] and index != 0]
 print(f"Обработанный список {new_list}")
