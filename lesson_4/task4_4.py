70a990a6d023dad33d6cc61967881446b23c0494
a2kat <mousedrw@ya.ru>
2021-01-27 17:07:10 +0300

    task4_4 add random

--- lesson_4/task4_4.py
+++ lesson_4/task4_4.py
@@ -3,6 +3,9 @@
 # Элементы выведите в порядке их следования в исходном списке.
 # Для выполнения задания обязательно используйте генератор.
 
-num_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+from random import randint
+
+num_list = [randint(0, 10) for i in range(14)]
+print(f"Изначальный список {num_list}")
 new_list = [i for i in num_list if num_list.count(i) == 1]
-print(new_list)
+print(f"Обработанный список {new_list}")
