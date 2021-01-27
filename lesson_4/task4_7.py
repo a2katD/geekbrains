ae1b47f69055ce6a1e896ac5aaa88b9dd4131336
a2kat <mousedrw@ya.ru>
2021-01-27 15:58:04 +0300

    task4_7 add 2 version

--- lesson_4/task4_7.py
+++ lesson_4/task4_7.py
@@ -4,6 +4,7 @@
 # Она отвечает за получение факториала числа.
 # В цикле нужно выводить только первые n чисел, начиная с 1! и до n!.
 
+# я не очень понял задание, сделал 2 варианта
 def fact(n):
     rez = 1
     for i in range(1, n + 1):
@@ -11,7 +12,27 @@ def fact(n):
         yield rez
 
 
-n = int(input("Введите число n: "))
+try:
+    n = int(input("Введите число n: "))
+except:
+    print("Вы вели не число")
+    quit()
 
 for i, el in enumerate(fact(n)):
     print(f"Факториал числа {i + 1}! = {el}")
+
+# вариант 2, тут сама функция факториал бесконечная, а n влияет только на цикл
+from itertools import count
+
+
+def factorial():
+    rez = 1
+    for i in count(1):
+        rez = rez * i
+        yield rez
+
+
+iterator = factorial()
+
+for i in range(n):
+    print(f"Второй вариант, Факториал числа {i + 1}! = {next(iterator)}")
