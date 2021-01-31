be2efde88f244e750665461095cfe277cbf4d02c
a2katD <mousedrw@ya.ru>
2021-01-31 11:40:07 +0300

    lesson5

--- lesson_5/task5_4.py
+++ lesson_5/task5_4.py
@@ -1,2 +1,8 @@
-with open("task5_4.txt", "r", encoding="utf-8") as file_r:
-    line = file_r.readline()
+transfer = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
+
+with open("task5_4_read.txt", "r", encoding="utf-8") as file_read:
+    with open("task5_4_write.txt", "w", encoding="utf-8") as file_write:
+        for line in file_read:
+            line = line.split()
+            line[0] = transfer[line[0]]
+            file_write.writelines(" ".join(line) + "\n")
---
+++ lesson_5/task5_7.py
@@ -0,0 +1,22 @@
+from json import dump
+
+profit = dict()
+average = {"average_profit": 0, "count": 0}
+
+with open("task5_7.txt", "r", encoding="utf-8") as f:
+    for company in f:
+        name, ownership, proceeds, damages = company.split()
+        profit[name] = int(proceeds) - int(damages)
+        if profit[name] > 0:
+            average["average_profit"] += profit[name]
+            average["count"] += 1
+
+average["average_profit"] = int(average["average_profit"] / average["count"])
+del average["count"]
+
+json_object = list()
+json_object.append(profit)
+json_object.append(average)
+
+with open("task5_7.json", "w", encoding="utf-8") as js:
+    dump(json_object, js, indent=4)
